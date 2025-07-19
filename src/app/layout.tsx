bcd9dcc2ce7c167c5d3fcff585f8a65aab66d64c
BlackMonkcr <aaron.navarro@utec.edu.pe>
2025-07-19 16:55:56 -0500

    fix merge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import '../styles/globals.css'
+import { TRPCReactProvider } from '~/trpc/react'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -14,7 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <TRPCReactProvider>
+          {children}
+        </TRPCReactProvider>
+      </body>
     </html>
   )
 }
